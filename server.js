cec9551dd5140519b448fc93891fd3cd191f9ac0
Intolerance72 <37460686+Intolerance72@users.noreply.github.com>
2026-09-12 08:44:10 +0000

    Add all homepage APIs

--- server.js
+++ server.js
@@ -17,3 +17,32 @@ console.log(`Server running on port ${PORT}`);
 app.get('/equity', (req, res) => {  
 res.json({ equity: 0 });
 });
+
+// API Profit & Loss
+app.get('/pnl', (req, res) => {
+res.json({ pnl: 0 });
+});
+
+// API Drawdown
+app.get('/drawdown', (req, res) => {
+res.json({ drawdown: 0 });
+});
+
+// API Operazioni Aperte
+app.get('/open_trades', (req, res) => {
+res.json({ open_trades: 0 });
+});
+
+// API Ordini Pendenti
+app.get('/pending_orders', (req, res) => {
+res.json({ pending_orders: 0 });
+});
+
+// API Ultima News Rossa (mini box Home)
+app.get('/latest_news', (req, res) => {
+res.json({
+title: "Nessuna news disponibile",
+impact: "none",
+time: null
+});
+});
